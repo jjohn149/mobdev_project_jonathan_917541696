9a394208f248333c430e647dc9d44b281a821dbd
Jonathan Johnson <j4.john149@gmail.com>
2024-10-16 11:25:59 -0400

    Implement original interface

---
+++ lib/home.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Easy Assist"),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlue,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Order Lunch"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Help"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Call My Loved Ones"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Interesting Places"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const AuthenticPage(),
+                  ),
+                );
+              },
+              child: const Text("Logout"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
